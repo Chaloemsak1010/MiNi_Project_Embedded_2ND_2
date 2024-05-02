9bef1b1bc8650c4a4ef609f3be97d43c0e9e909c
chaloemsak101 <112938790+Chaloemsak1010@users.noreply.github.com>
2024-05-02 09:31:16 +0700

    Update testToBeSubscriberTopic.js

--- testToBeSubscriberTopic.js
+++ testToBeSubscriberTopic.js
@@ -3,7 +3,7 @@
 // ======================= Dev: Mike016 ======================= //
 //////////////////////////////////////////////////////////////////
 
-// Test to be subscriber (run on pc)
+// Test to be subscriber (Open new vs code to run this code on your pc)
 const mqtt = require("mqtt");
 // MQTT options
 const mqttOptions = {
