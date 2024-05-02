2fd1c7933ecf8b01b732c89b4a958bc7d8a1deb8
chaloemsak101 <112938790+Chaloemsak1010@users.noreply.github.com>
2024-05-02 09:30:01 +0700

    Add files via upload

---
+++ testToBeSubscriberTopic.js
@@ -0,0 +1,38 @@
+// Author : Chaloemsak Arsung
+//////////////////////////////////////////////////////////////////
+// ======================= Dev: Mike016 ======================= //
+//////////////////////////////////////////////////////////////////
+
+// Test to be subscriber (run on pc)
+const mqtt = require("mqtt");
+// MQTT options
+const mqttOptions = {
+    host: "YOURhivemqtthostURL.s1.eu.hivemq.cloud",
+    port: 8883,
+    protocol: "mqtts",
+    username: "usernameAccess",
+    password: "password",
+  };
+// Initialize the MQTT client
+const mqttClient = mqtt.connect(mqttOptions);
+// Setup MQTT client callbacks
+mqttClient.on("connect", function () {
+  console.log("Connected to MQTT broker");
+});
+// Detect error
+mqttClient.on("error", function (error) {
+  console.error("MQTT Error:", error);
+});
+
+//Subscribe to the 'temp' topic
+mqttClient.on("message", function (topic, message) {
+    console.log('topic: ' ,topic.toString(),' message arrived :', message.toString());
+});
+// sub temp topic from hive mqtt clound for test to be subscriber
+mqttClient.subscribe("fan");
+mqttClient.subscribe("led");
+
+console.log("Author : Chaloemsak Arsung");
+console.log('//////////////////////////////////////////////////////////////////');
+console.log("// ======================= Dev: Mike016 ======================= //");
+console.log('//////////////////////////////////////////////////////////////////');
